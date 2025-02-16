e3cc2f7e7b8a9cde535af741bb953b5e1aa68aa8
Ishita <nope.ishita@gmail.com>
2025-02-16 21:34:25 +0530

    restraunts routing done

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template , redirect , url_for
 
 app = Flask(__name__)
 
+#ROUTING
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,6 +19,30 @@ def payment():
 def login():
     return render_template('login.html')
 
+@app.route('/taj')
+def taj():
+    return render_template('TajPalace.html')
 
+@app.route('/punjab')
+def punjab():
+    return render_template('PunjabGrill.html')
+
+@app.route('/dosa')
+def dosa():
+    return render_template('DosaPlaza.html')
+
+@app.route('/biryani')
+def biryani():
+    return render_template('BiryaniHouse.html')
+
+@app.route('/sarwana')
+def sarwana():
+    return render_template('SarwanaBhawan.html')
+
+@app.route('/mughal')
+def mughal():
+    return render_template('MughalsKitchen.html')
+
+#run app
 if __name__ == '__main__':
     app.run(debug=True)
